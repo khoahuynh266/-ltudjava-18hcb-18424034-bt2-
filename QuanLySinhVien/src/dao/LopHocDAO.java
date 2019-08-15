dd592ac8676f942abbd2412f81d222dc1c98129b
khoahuynh <yumiling1001@gmail.com>
2019-08-16 05:54:14 +0700

    TKB + LopHocDAO

---
+++ QuanLySinhVien/src/dao/LopHocDAO.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import pojo.LopHoc;
+import pojo.ThoiKhoaBieu;
+import ulti.HibernateUtil;
+
+
+/**
+ *
+ * @author yumil
+ */
+public class LopHocDAO {
+     public static List<LopHoc> getListLopHoc() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        List<LopHoc> listLopHoc = null;
+        try {
+            String hql = "select lh from LopHoc lh";
+            Query query = session.createQuery(hql);
+            listLopHoc = query.list();
+        } catch (HibernateException ex) {
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+
+        return listLopHoc;
+    }
+    
+    public static ArrayList<ThoiKhoaBieu> getListThoiKhoaBieu(String tenLop) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        ArrayList<ThoiKhoaBieu> listThoiKhoaBieu = null;
+        try {
+            String hql = "select tkb"; 
+            hql += " from ThoiKhoaBieu tkb";
+            hql += " where tkb.tenLop=:tenLop";
+            Query query = session.createQuery(hql);
+            query.setParameter("tenLop", tenLop);
+            listThoiKhoaBieu = (ArrayList<ThoiKhoaBieu>) query.list();
+        } catch (HibernateException ex) {
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+
+        return listThoiKhoaBieu;
+    }
+
+    public static LopHoc getLopHoc(String tenLop) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        LopHoc lh = null;
+        try {
+            lh = (LopHoc) session.get(LopHoc.class, tenLop);
+        } catch (HibernateException ex) {
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+        return lh;
+    }
+
+    public static boolean themLopHoc(LopHoc lh) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        if(LopHocDAO.getLopHoc(lh.getTenLop()) != null) {
+            return false;
+        }
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(lh);
+            transaction.commit();
+        } catch(HibernateException ex) {
+            transaction.rollback();
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+        return true;
+    }
+}
--- QuanLySinhVien/src/pojo/ThoiKhoaBieu.java
+++ QuanLySinhVien/src/pojo/ThoiKhoaBieu.java
@@ -10,33 +10,71 @@ package pojo;
  * @author yumil
  */
 public class ThoiKhoaBieu implements java.io.Serializable {
-    private MonHoc monHoc;
-    private String phongHoc;
+    private int id;
+    private String maMonHoc;
+    private String tenMonHoc;
+    private String tenLop;
+     private String phongHoc;
+     private String hocKy;
+     private String namHoc;
     
     public ThoiKhoaBieu(){
-        this.monHoc.setMaMH("");
-        this.monHoc.setTenMH("");
-        this.phongHoc = "";
-    }
-    
-    public ThoiKhoaBieu(MonHoc mh, String ph){
-        this.monHoc = mh;
-        this.phongHoc = ph;
+        this.maMonHoc= "";
+        this.tenMonHoc= "";
+        this.tenLop = "";
+           this.phongHoc= "";
+        this.hocKy = "";
+           this.namHoc= "";
+    } 
+    public ThoiKhoaBieu(String maMonHoc, String tenMonHoc, String tenLop, String phongHoc, String hocKy, String namHoc){
+        this.maMonHoc= "";
+        this.tenMonHoc= "";
+        this.tenLop = "";
+           this.phongHoc= "";
+        this.hocKy = "";
+           this.namHoc= "";
     }
      
-    public MonHoc getMH(){
-        return this.monHoc;
+    public String getMaMonHoc() {
+        return this.maMonHoc;
     }
     
-    public void setMH(MonHoc mh){
-        this.monHoc = mh;
+    public void setMaMonHoc(String maMonHoc) {
+        this.maMonHoc = maMonHoc;
+    }
+    public String getTenMonHoc() {
+        return this.tenMonHoc;
     }
     
-    public String getPH(){
+    public void setTenMonHoc(String tenMonHoc) {
+        this.tenMonHoc = tenMonHoc;
+    }
+    public String getMaLop() {
+        return this.tenLop;
+    }
+    
+    public void setMaLop(String tenLop) {
+        this.tenLop = tenLop;
+    }
+    public String getPhongHoc() {
         return this.phongHoc;
     }
     
-    public void setPH(String phong){
-        this.phongHoc = phong;
+    public void setPhongHoc(String phongHoc) {
+        this.phongHoc = phongHoc;
+    }
+    public String getHocKy() {
+        return this.hocKy;
+    }
+    
+    public void setHocKy(String hocKy) {
+        this.hocKy = hocKy;
+    }
+    public String getNamHoc() {
+        return this.namHoc;
+    }
+    
+    public void setNamHoc(String namHoc) {
+        this.namHoc = namHoc;
     }
 }
